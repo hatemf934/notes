178fd8d113d5805eb582fb47d6053fa260446469
hatemfathy <hatemf934@gmail.com>
2025-02-16 02:31:36 +0200

    create model and model-Adapter

---
+++ lib/models/note_model.dart
@@ -0,0 +1,23 @@
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String subTitle;
+  @HiveField(2)
+  final int color;
+  @HiveField(3)
+  final int number;
+  @HiveField(4)
+  final String date;
+
+  NoteModel(
+      {required this.title,
+      required this.subTitle,
+      required this.color,
+      required this.number,
+      required this.date});
+}
